4340529fd2909badf9c5967238d92391d8b98603
Creosino <creosino@gmail.com>
2016-08-21 01:50:23 -0400

    inital commit

---
+++ src/main.java
@@ -0,0 +1,34 @@
+
+public class main {
+	static void reverseEachWordOfString(String inputString) {
+		String[] words = inputString.split(" ");
+
+		String reverseString = "";
+
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];
+
+			String reverseWord = "";
+
+			for (int j = word.length()-1; j >= 0; j--) {
+				reverseWord = reverseWord + word.charAt(j);
+			}
+
+			reverseString = reverseString + reverseWord + " ";
+		}
+
+		System.out.println(inputString);
+
+		System.out.println(reverseString);
+
+		System.out.println("-------------------------");
+	}
+
+	public static void main(String[] args) {
+		reverseEachWordOfString("demo and proof of concept");
+
+		reverseEachWordOfString("Reversing each word in a string is easy");
+
+	}
+
+}
